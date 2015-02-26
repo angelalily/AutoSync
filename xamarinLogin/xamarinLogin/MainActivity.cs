ed9c5670735b760d691ce73c69a60cf691381404
angelalily <adsbox2014@gmail.com>
2015-02-25 22:33:17 -0600

    test file with xamarinlogin

---
+++ xamarinLogin/xamarinLogin/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace xamarinLogin
+{
+    [Activity(Label = "xamarinLogin", MainLauncher = true, Icon = "@drawable/LionUp")]
+    public class MainActivity : Activity
+    {
+        
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            //Initializing button from layout
+            Button login = FindViewById<Button>(Resource.Id.login);
+
+            //Login button click action
+            login.Click += (object sender, EventArgs e) =>
+            {
+                Android.Widget.Toast.MakeText(this, "Login Button Clicked!", Android.Widget.ToastLength.Short).Show();
+            };
+        }
+    }
+}
+
